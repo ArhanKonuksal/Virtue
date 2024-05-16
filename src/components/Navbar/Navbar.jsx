002751fad52f55b8b2f656c5100276b68f361803
ArhanKonuksal <arhankonuksal@gmail.com>
2024-05-16 16:24:12 +0300

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,52 @@
-import React, {useState} from 'react'
-import {Link} from "react-router-dom"
-import {FaBars, FaTimes} from "react-icons/fa"
-import "./Navbar.css"
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../../assets/logo.png";
+import LanguageLogo from "../../assets/language.png";
+import "./Navbar.css";
 
 const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
 
-    const [click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
-
-    const [color, setColor] = useState(false)
-    const changeColor = () => {
-        if (window.scrollY >= 100) {setColor(true)} else {
-            setColor(false)
-    }
-}
-
-    window.addEventListener("scroll", changeColor)
-
-  return (
-    <div className={color ? "header header-bg" : "header"}>
-        <Link to="/">
-        </Link>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li>
-                <Link to="/Home">Home</Link>
-            </li>
-            <li>
-                <Link to="/About">About</Link>
-            </li>
-            <li>
-                <Link to="/Contact">Contact</Link>
-            </li>
-            <li>
-                <Link to="/JoinUs">JoinUs</Link>
-            </li>
-            <li>
-                <Link to="/Services">Services</Link>
-            </li>
-        </ul>
-        <div className="hamburger" onClick={handleClick}>
-            {click ? (
-                <FaTimes size={20} style={{color:
-                "#fff"}}/>
-                ):(<FaBars size={20} style={{color: "#fff"
-             }} />
-            )}
+    return (
+        <div className="header">
+            <div className="hamburger" onClick={handleClick}>
+                {click ? (
+                    <FaTimes size={20} style={{ color: "#fff" }} />
+                ) : (
+                    <FaBars size={20} style={{ color: "#fff" }} />
+                )}
+            </div>
+            <div className={click ? "nav-menu active" : "nav-menu"}>
+                <ul className="nav-menu-list">
+                    <li>
+                        <Link to="/Home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/About">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/Contact">Contact</Link>
+                    </li>
+                    <li>
+                        <Link to="/JoinUs">JoinUs</Link>
+                    </li>
+                    <li>
+                        <Link to="/Services">Services</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="logo">
+                <Link to="/">
+                    <img src={Logo} alt="Logo" />
+                </Link>
+            </div>
+                <div className="language-button">
+                    <button><img src={LanguageLogo} alt='logo'/></button>
+                </div>
         </div>
-    </div>
-  )
-}
-export default Navbar
+    );
+};
+
+export default Navbar;
