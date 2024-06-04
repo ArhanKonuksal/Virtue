050509b907798f5a89664cafa3d1952e531ed3b3
ArhanKonuksal <arhankonuksal@gmail.com>
2024-06-04 22:55:38 +0300

    present

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './Hero.css';
 
-const Hero = (props) => {
+const Hero = () => {
   return (
-    <div className="hero">
-    <div className={props.cName}>
-      <img alt="HeroImg" src={props.heroImg} />
-      <div className="hero-text">
-        <h1>{props.title}</h1>
-        <p>{props.text}</p>
-        {props.buttonText && <a href={props.buttonLink} className="show">{props.buttonText}</a>}
+    <div className="title">
+      <div className="text-content">
+        <h1>Virtue.</h1>
+        <h2>Always Forward with the JEWS</h2>
       </div>
-    </div>
+      <div className="video-overlay">
+        <video className="background-video" autoPlay loop muted>
+        </video>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import Footer from "../components/Footer/Footer"
 import Navbar from "../components/Navbar/Navbar"
 import Hero from "../components/Hero/Hero"
-import HomePhoto from "../assets/Homepage.png"
 
 const Home = () => {
   return (
   <>
   <div>
     <Navbar />
-    <Hero cName="hero-mid" heroImg={HomePhoto}/>
+    <Hero />
     <Footer />
     </div>
   </>
