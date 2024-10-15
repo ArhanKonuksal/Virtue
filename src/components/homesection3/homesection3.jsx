39f8337febb261e7114b49014c50188eace25057
ArhanKonuksal <arhankonuksal@gmail.com>
2024-10-15 09:14:35 +0200

    Home page finished finished without css and homesection1 css added

--- src/components/homesection3/homesection3.jsx
+++ src/components/homesection3/homesection3.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
 import "./homesection3.css";
-import VirtueLogo from "../../assets/Virtue-Logo.png"
-import BlueRectangle from "../../assets/BlueRectangle.png"
-import CodeBlock from "../../assets/codeBlock.png"
-import NpmBlock from "../../assets/npm-requestfoto.png"
+import VirtueLogo from "../../assets/Virtue-Logo.png";
+import BlueRectangle from "../../assets/BlueRectangle.png";
+import CodeBlock from "../../assets/codeBlock.png";
+import NpmBlock from "../../assets/npm-requestfoto.png";
 
-const homesection3 = () => {
+const HomeSection3 = () => {
   return (
     <>
-    <div className='home-section3-container'>
+      <div className='home-section3-container'>
         <div className='headings'>
-            <h1>Elevated User Experiences by Virtue</h1>
-            <h1>UI/UX</h1>
-            <h1>Integrated UX Design Solutions (blue)</h1>
+          <h1>Elevated User Experiences by Virtue</h1>
+          <h1>UI/UX</h1>
+          <h1>Integrated UX Design Solutions (blue)</h1>
         </div>
-            <p>Modular Development Framework: Virtue's approach empowers developers to streamline UX/UI development, leveraging preferred frameworks and existing tech stacks for seamless deployment.</p>
+        <p>
+          Modular Development Framework: Virtue's approach empowers developers to streamline UX/UI development, leveraging preferred frameworks and existing tech stacks for seamless deployment.
+        </p>
         <div className='image-container'>
-            <img src={VirtueLogo} alt="virtue-logo" />
-            <img src={BlueRectangle} alt='Blue'>
+          <img src={VirtueLogo} alt="virtue-logo" />
+          
+          <div className="blue-rectangle-content">
+            <img src={BlueRectangle} alt='Blue Rectangle' />
             <h1>Accelerate Your Design Process</h1>
-            <p>Virtue isn't just a hub for innovative solutions. It’s a platform crafted to streamline the future of intuitive user experience designs. 
-
-Jumpstart your application with our user-centric design tools and libraries, enabling rapid prototyping and implementation with just a few clicks. Make Virtue your go-to for deploying user interfaces that engage and convert.</p>
+            <p>
+              Virtue isn't just a hub for innovative solutions. It’s a platform crafted to streamline the future of intuitive user experience designs.
+              Jumpstart your application with our user-centric design tools and libraries, enabling rapid prototyping and implementation with just a few clicks.
+              Make Virtue your go-to for deploying user interfaces that engage and convert.
+            </p>
             <a href='/readmore'>READ MORE</a>
-            </img>
-            <img src={CodeBlock} alt='CodeBlock'/>
-            <img src={NpmBlock} alt='Npm Block'/>
-        </div>
-    </div>
-    </>
-  )
-}
+          </div>
 
-export default homesection3
+          <img src={CodeBlock} alt='Code Block'/>
+          <img src={NpmBlock} alt='Npm Block'/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeSection3;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,28 +3,27 @@ import Footer from "../components/Footer/Footer"
 import Navbar from "../components/Navbar/Navbar"
 import Hero from "../components/Hero/Hero"
 import OurClients from '../components/Our Clients/OurClients'
-//import homesection1 from '../components/homesection1/homesection1'
-//import homesection2 from '../components/homesection1/homesection2'
-//import homesection3 from '../components/homesection1/homesection3'
-//import homesection4 from '../components/homesection1/homesection4'
-//import homesection5 from '../components/homesection1/homesection5'
-//import Divider from '../components/Divider/Divider'
+import Homesection1 from '../components/homesection1/homesection1'
+import Homesection2 from '../components/homesection2/homesection2'
+import Homesection3 from '../components/homesection3/homesection3'
+import Homesection4 from '../components/homesection4/homesection4'
+import Homesection5 from '../components/homeseciton5/homesection5'
+import Divider from '../components/Divider/Divider'
+import DividerImg from "../assets/HomePageDivider2.png"
 
 const Home = () => {
   return (
   <>
-  <div>
     <Navbar />
     <Hero />
     <OurClients />
-    {/*
     <Homesection1 />
     <Homesection2 />
+    <Homesection3 />
     <Homesection4 />
-    <Divider />
-    <Homesection5 />*/}
+    <Divider DividerImg={DividerImg} title="Bridging Business Gaps" description="Virtue's Integrated Marketing Strategies"/>
+    <Homesection5 /> 
     <Footer />
-    </div>
   </>
   )
 }
