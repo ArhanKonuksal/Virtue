f0bc4a964e68f7232bb37345035116103c495f0e
ersoybaki <ersoyatabaki@gmail.com>
2024-12-05 22:25:16 +0100

    mainpage first part fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import Navbar from "../components/Navbar/Navbar"
-import Footer from "../components/Footer/Footer"
-import "../styles/homedividerstyle.css"
-import Hero from "../components/Hero/Hero"
-import OurClients from '../components/Our Clients/OurClients'
-import Homesection1 from '../components/home/homesection1/homesection1'
-import Homesection2 from '../components/home/homesection2/homesection2'
-import Homesection3 from '../components/home/homesection3/homesection3'
-import Homesection4 from '../components/home/homesection4/homesection4'
-import Homesection5 from '../components/home/homeseciton5/homesection5'
-import DividerPhoto from '../assets/HomePageDivider2.png'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import "../styles/homedividerstyle.css";
+import Hero from "../components/Hero/Hero";
+import OurClients from "../components/Our Clients/OurClients";
+import Homesection1 from "../components/home/homesection1/homesection1";
+import Homesection2 from "../components/home/homesection2/homesection2";
+import Homesection3 from "../components/home/homesection3/homesection3";
+import Homesection4 from "../components/home/homesection4/homesection4";
+import Homesection5 from "../components/home/homeseciton5/homesection5";
+import DividerPhoto from "../assets/HomePageDivider2.png";
 
 const Home = () => {
   return (
-  <>
-    <Navbar />
-    <Hero />
-    <OurClients />
-    <Homesection1 />
-    <Homesection2 />
-    <Homesection3 />
-    <Homesection4 />
-    <div className="divider">
-      <img alt="DividerImg" src={DividerPhoto} />
-      <div className="divider-text">
-        <h1>Bridging Business Gaps</h1>
-        <p>Virtue's Integrated Marketing Strategies</p>
+    <>
+      <Navbar />
+      <Hero />
+      <OurClients />
+      <Homesection1 />
+      <Homesection2 />
+      <Homesection3 />
+      <Homesection4 />
+      <div className="divider">
+        <img alt="DividerImg" src={DividerPhoto} />
+        <div className="divider-text">
+          <h1>Bridging Business Gaps</h1>
+          <p>Virtue's Integrated Marketing Strategies</p>
+        </div>
       </div>
-    </div>
-    <Homesection5 /> 
-    <Footer />
-  </>
-  )
-}
+      <Homesection5 />
+      <Footer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
